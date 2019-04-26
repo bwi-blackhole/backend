30b72a86b4116b6b2089e3fab6bb40128132dfcd
Bao Pham <ryanvuepham@gmail.com>
2019-04-25 21:35:04 -0500

    userTable created

---
+++ data/migrations/20190425211853_usersTable.js
@@ -0,0 +1,20 @@
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('users', tbl => {
+		tbl.increments()
+		tbl
+			.string('username', 256)
+			.notNullable()
+			.unique()
+		tbl
+			.string('email', 256)
+			.notNullable()
+			.unique()
+		tbl.string('password', 256).notNullable()
+		tbl.string('firstName', 256)
+		tbl.string('lastName', 256)
+	})
+}
+
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTableifExists('users')
+}
