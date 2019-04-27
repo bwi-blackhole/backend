04aee07b53052c1cdcbfc32831585c2f3dd15686
Bao Pham <ryanvuepham@gmail.com>
2019-04-26 20:47:32 -0500

    ...

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,15 @@
 module.exports = {
 	development: {
 		client: 'sqlite3',
+		useNullAsDefault: true,
 		connection: {
 			filename: './data/blackhole.db3'
 		},
-		useNullAsDefault: true,
+		pool: {
+			afterCreate: (conn, done) => {
+				conn.run('PRAGMA foreign_keys = ON', done)
+			}
+		},
 		migrations: {
 			directory: './data/migrations'
 		},
