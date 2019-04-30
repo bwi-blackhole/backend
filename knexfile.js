d4fffbfaa017a4a1e8c9c0d3fed5de3014d9343c
yannafaith <yannafaith97@gmail.com>
2019-04-29 19:42:42 -0700

    adds necessities for pg db

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,10 @@
 // Update with your config settings.
 
+/* need this or get "no pg_hba.conf entry for host" error */
+const pg = require('pg');
+pg.defaults.ssl = true;
+// npx knex migrate:latest --env production
+
 module.exports = {
 	development: {
 		client: 'sqlite3',
@@ -18,6 +23,22 @@ module.exports = {
 		seeds: {
 			directory: './data/seeds'
 		}
+	},
+	production: {
+		client: 'pg',
+		 /* process.env.DATABASE_URL || */ 
+		connection: 'postgres://cgukeenlsiokhh:4847c39244669435beb3e9c4dadcecf01db57b10fddd1d950d33a72d0212f1e9@ec2-174-129-208-118.compute-1.amazonaws.com:5432/d1vlkm64fr653c',
+		ssl: true,
+		pool: {
+		  min: 2,
+		  max: 50,
+		},
+		migrations: {
+			directory: './data/migrations'
+		},
+		seeds: {
+			directory: './data/seeds'
+		},
 	},
 	testing: {
 		// testing object can be any name you choose
